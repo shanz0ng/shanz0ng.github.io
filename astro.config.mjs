05d64b85f5027134b570b8fb9e18dcea96507699
山宗 <shanz0ng@users.noreply.github.com>
2026-06-16 21:04:21 +0800

    添加文章侧边栏目录导航

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
+import rehypeSlug from "rehype-slug";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
 
@@ -25,6 +26,7 @@ export default defineConfig({
         },
       ],
     ],
+    rehypePlugins: [rehypeSlug],
     shikiConfig: {
       theme: "one-dark-pro",
       wrap: true,
